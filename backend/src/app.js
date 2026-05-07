3341f3a6ddbfd4ae45b3e282759e8e49f1a1ce2e
abhinav-973 <vabhinav898@gmail.com>
2026-05-07 16:19:34 +0530

    create user controller and user schema

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
 const app = express();
-app.use(cookieParser());
+
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
@@ -13,5 +13,15 @@ app.use(
 
 app.use(express.json({ limit: "16kb" }));
 app.use(urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//routes
+import userRouter from './routes/user.routes.js';
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
 
 export default app;
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,114 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+
+    role: {
+      type: String,
+      enum: ["student", "admin"],
+      default: "student",
+    },
+
+    resumeUrl: {
+      type: String,
+      default: "",
+    },
+
+    skills: [
+      {
+        type: String,
+      },
+    ],
+
+    targetRole: {
+      type: String,
+      default: "",
+    },
+
+    experienceLevel: {
+      type: String,
+      enum: ["beginner", "intermediate", "advanced"],
+      default: "beginner",
+    },
+
+    interviewsTaken: {
+      type: Number,
+      default: 0,
+    },
+
+    averageScore: {
+      type: Number,
+      default: 0,
+    },
+
+    refreshToken: {
+      type: String,
+      default: "",
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified("password")) {
+      return next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      userId: this._id,
+      email: this.email,
+      role: this.role,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRATION,
+    },
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign({ userId: this._id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRATION,
+  });
+};
+
+export const User = mongoose.model("User", userSchema);
