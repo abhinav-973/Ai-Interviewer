0cea687485e5965d730f79a35ae7258937da50af
abhinav-973 <vabhinav898@gmail.com>
2026-05-09 18:12:54 +0530

    add changePassword and getCurrentUser functions to user.controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -146,26 +146,25 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
       incomingRefreshToken,
       process.env.REFRESH_TOKEN_SECRET,
     );
-  
+
     const user = await User.findById(decodedToken?.userId);
-  
+
     if (!user) {
       throw new ApiError(404, "Invalid refresh token");
     }
-  
+
     if (user?.refreshToken !== incomingRefreshToken) {
       throw new ApiError(401, "Refresh token does not match");
     }
-  
+
     const options = {
       httpOnly: true,
       secure: true,
     };
-  
-    const { accessToken, newRefreshToken } = await generateAccessAndRefreshTokens(
-      user._id,
-    );
-  
+
+    const { accessToken, newRefreshToken } =
+      await generateAccessAndRefreshTokens(user._id);
+
     return res
       .status(200)
       .cookie("accessToken", accessToken, options)
@@ -177,10 +176,34 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
           "Access token refreshed successfully",
         ),
       );
-  
   } catch (error) {
     throw new ApiError(401, "Invalid refresh token");
   }
 });
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken };
+const changePassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  const user = await User.findById(req.user._id);
+
+  const isOldPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+  if (!isOldPasswordCorrect) {
+    throw new ApiError(400, "Invalid old password");
+  }
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"));
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "Current user fetched successfully"));
+});
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken, changePassword, getCurrentUser };
